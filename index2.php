49b3416dab5f5fe8fedaf6cd829ae1d85eeac154
chenss3 <sophia.s.chen@vanderbilt.edu>
2021-04-08 17:24:40 -0500

    Create a working mega table

--- index2.php
+++ index2.php
@@ -1,94 +1,67 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> HTML Cheat Sheet </title>
+        <title> kayla </title>
     </head>
     <body>
-        <h1> Heading One </h1>
-        <h2> Heading Two</h2>
+        <h1> Jack and Sophia's amazing project yay </h1>
+        <?php
+        try {
+        $dbhost = '127.0.0.1'; // localhost
+        $dbuname = 'root'; //user name
+        $dbpass = 'root'; //Password
+        $dbname = 'parkingviolations'; //Database name
+
+        $dbo = new PDO('mysql:host=' . $dbhost . ';port=8889;dbname=' . $dbname, $dbuname, $dbpass); ?>
         <table>
-            <tr>
+            <!-- Create the first row of table as table head (thead). -->
+            <thead>
+                <!-- The top row is table head with four columns named -- summons_number, plate_id ... -->
+                <tr>
                 <th>summons_number</th>
                 <th>plate_id</th>
                 <th>registration_state</th>
-            </tr>
-            <?php
-
-            try {
-            $dbhost = '127.0.0.1'; // localhost
-            $dbuname = 'root';
-            $dbpass = 'root';
-            $dbname = 'parkingviolations'; //Database name
-
-            $dbo = new PDO('mysql:host=' . $dbhost . ';port=8889;dbname=' . $dbname, $dbuname, $dbpass);
-            foreach($dbh->query('SELECT * from ParkingViolationsMegaTable LIMIT 5') as $row) {
-                print_r($row);
-            }
-            $dbh = null;
-            } catch (PDOException $e) {
-                print "Error!: " . $e->getMessage() . "<br/>";
-                die();
-            }
-
-            
-            /*
-            $host="127.0.0.1";
-            $username = "root";
-            $password = "";
-            $database = "parkingviolations";
-            
-            $mysqli = new mysqli($host,$username,$password, $database);
-
-
-            if ($mysqli->connect_error) {
-                die("Connection failed: " . $mysqli->connect_error);
-                exit();
-            } else {
-                echo "Connect Succesfully. Host info: " . $mysqli->host_info;
-            }
-            */
-
-            /*
-            $host='localhost';
-            $username = "root";
-            $password = "";
-            $database = "parkingviolations";
-            $con=mysqli_connect($host,$user,$pass, $database);
-            if($con === false){
-                die("ERROR: Could not connect." . $mysql->connect_error);
-            }
-            echo "Connect Succesfully. Host info " . $mysql->host_info; */
-            $summons_number = 'summons_number';
-            $plate_id= 'plate_id';
-            $registration_state = 'registration_state';
-            $q="SELECT summons_number, plate_id, registration_state FROM ParkingViolationsTable LIMIT 10";
-            $r=mysqli_query($mysqli,$q);
-            var_dump($r);
-            while($rows=mysqli_fetch_assoc($r)){
-                echo "summons_number :".$rows[$summons_number]."<br />"."plate_id :".$rows[$plate_id]."<br />"."registration_state :".$rows[$registration_state]."<br />";
-            }
-            $mysqli -> close();
-            /*
-            $conn = mysqli_connect("localhost", "root", "", "parkingviolations");
-
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            } else {
-                echo "<p>Connected to MySQL!</p>";
-            }
-
-            $sql = "SELECT summons_number, plate_id, registration_state FROM ParkingViolationsMegaTable";
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    echo "<tr><td>" . $row["summons_number"]. "</td><td>" . $row["plate_id"] . "</td><td>". $row["registration_state"]. "</td></tr>";
-                }
-            echo "</table>";
-            } else { echo "0 results"; }
-            $conn->close();
-            */
-            ?>
+                <th>plate_type</th>
+                <th>issue_date</th>
+                <th>vehicle_body_type</th>
+                <th>vehicle_make</th>
+                <th>issuing_agency</th>
+                <th>street_code1</th>
+                </tr>
+            </thead>
+                <!-- Create rest of the the body of the table -->
+            <tbody>
+                <!-- For each row returned by the query -->
+                <?php foreach ($dbo->query('SELECT summons_number, plate_id, registration_state, plate_type, issue_date, violation_code, 
+                                                    vehicle_body_type, vehicle_make, issuing_agency, street_code1
+                                            from ParkingViolationsMegaTable LIMIT 10') as $row) { ?>
+                <tr>
+                    <!-- Print (echo) the value of each column of the table -->
+                    <td><?php echo $row["summons_number"]; ?></td>
+                    <td><?php echo $row["plate_id"]; ?></td>
+                    <td><?php echo $row["registration_state"]; ?></td>
+                    <td><?php echo $row["plate_type"]; ?></td>
+                    <td><?php echo $row["issue_date"]; ?></td>
+                    <td><?php echo $row["violation_code"]; ?></td>
+                    <td><?php echo $row["vehicle_body_type"]; ?></td>
+                    <td><?php echo $row["vehicle_make"]; ?></td>
+                    <td><?php echo $row["issuing_agency"]; ?></td>
+                    <td><?php echo $row["street_code1"]; ?></td>
+                <!-- End first row. Note this will repeat for each row returned by the previous query-->
+                </tr>
+                <?php } 
+                //Set it to null since we are done with the variable 
+                $dbo = null ?>
+                <!-- End table body -->
+            </tbody>
+            <!-- End table -->
         </table>
+
+        <?php $dbo = null;
+        } catch (PDOException $e) {
+            //Will tell us our error message if the connection doesn't work etc
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        } ?> 
     </body>
 </html>
